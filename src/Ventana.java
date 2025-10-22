91085d193139d29c7bc809176e6857331b4f0598
RichardSSJ <richard.soria@udla.edu.ec>
2025-10-22 16:37:09 -0500

    Creación de los elementos de la interfaz gráfica

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+
 public class Ventana {
+    private JTabbedPane tabbedPane1;
+    private JPanel principal;
+    private JSpinner spiIdentificadorTarea;
+    private JTextField txtNombreTarea;
+    private JComboBox cboCategoriaTarea;
+    private JTextField txtPresupuestoTarea;
+    private JComboBox cboPrioridad;
+    private JTextArea txtListarTarea;
+    private JButton btnEcolarTarea;
+    private JLabel lblPresupuestoCategoria;
+    private JLabel lblPresupuestoAjustadoCategoria;
 }
